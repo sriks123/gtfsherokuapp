ecbcfedb088a3e41129d8039b63d757de608bcc6
sriks123 <srksriks123@gmail.com>
2018-06-20 14:56:59 +0530

    Change procfile

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -22,14 +22,9 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
 ]
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES['default'] =  dj_database_url.config()
