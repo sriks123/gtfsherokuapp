09eafc355b9a32ce6929139b631fae4876e23a02
sriks123 <srksriks123@gmail.com>
2018-06-20 15:07:40 +0530

    Change procfile

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -27,7 +27,6 @@ DATABASES = {
     )
 }
 DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
